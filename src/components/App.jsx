d803f7813d9797c257e5dfde0d8d0d403ad27b9e
Lisovyi-Vladyslav <dragodrift2005@gmail.com>
2023-02-13 15:00:15 +0200

    СOM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Statistics } from 'components/Statistics/Statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Section } from 'components/Section/Section'
+import { Notification } from 'components/Notification/Notification'
+import { Component } from 'react';
+import ReactDOM from "react-dom";
+import feedbackWidget from 'feedbackWidget.json';
+
+ export class App extends Component {
+   state = {
+  good: 0,
+  neutral: 0,
+     bad: 0,
+     total: 0,
+     positiveFeedbackPercentage: 0,
+   }
+   
+   countPositiveFeedbackPercentage = () => {
+  this.setState(prevState => {
+    return {positiveFeedbackPercentage:  Math.round((prevState.good * 100) / prevState.total.toFixed(5)) + "%"}
+  });
+
+  };
+
+   countTotalFeedback = () => {
+   
+     
+     this.setState(prevState => {
+      return {total: prevState.good + prevState.neutral + prevState.bad}
+    });
+    
+      this.countPositiveFeedbackPercentage();
+  } 
+
+   handleClick = event => {
+     const { name } = event.target;
+     this.setState(prevState => ({
+       [name]: prevState[name] + 1,
+     }));
+      this.countTotalFeedback();
+   };
+   
+  render() {
+    const { good, neutral, bad, total, positiveFeedbackPercentage } = this.state;
+console.log(this.state);
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions  onLeaveFeedback={this.handleClick}/> 
+        </Section>
+         {total === 0 ? (
+        <Notification message="There is no feedback"/>
+      ) : (
+        <Section title="Statics">
+          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positiveFeedbackPercentage} />
+        </Section>
+      )}
+      </>
+          
+    );
+  }
+}
